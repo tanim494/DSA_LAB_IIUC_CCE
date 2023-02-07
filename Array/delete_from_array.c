c37cef4e92f03f2979006a93c8aa1be93ae199d5
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-02-07 13:40:00 +0600

    Create delete_from_array.c

---
+++ Array/delete_from_array.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+ * Author: Ahsanul Karim Tanim
+ * ID: E221013
+ * Experiment-3
+ * Experiment Task:  Write a C program to Delete an element from an Array.
+ * Github: github.com/tanim494
+*/
+void main() {
+    int size;
+    printf("Enter the size of array: ");
+    scanf("%d",&size);
+    int array[size];    //creating a array of the size that user want
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter %d-no element: ",i+1);
+        scanf("%d",&array[i]);  //running a loop for inserting elements in array
+    }
+    printf("Array before changing:  ");
+    for (int j = 0; j < size; j++)
+    {
+        printf("%d  ",array[j]);    //printing array before changing
+    }
+    printf("\nWhich element you want to delete:");
+    int d;
+    scanf("%d",&d);
+    for (int k = d - 1; k < size - 1; k++)
+    {
+        array[k] = array[k+1];  //deleting the element that user wants
+    }
+    printf("Array after changing: ");
+    for (int l = 0; l < size - 1; l++)
+    {
+        printf("%d  ",array[l]);    //printing the array after deleting
+    }
+    
+}
